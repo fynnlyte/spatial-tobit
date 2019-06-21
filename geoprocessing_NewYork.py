ccb1eedfcf4e39de523b0363de145331aa281b8a
TheX9 <51077606+TheX9@users.noreply.github.com>
2019-06-21 17:16:02 +0200

    Daten sind ready
    Die Daten für NewYork sind erstmal fertig. Habe auch das Python script fürs geoprocessing mit hochgeladen. Das bringt allerdings nur mit arcpy was.
    Mit import_data.py könnte ihr die Daten importieren und habt dann die Adjazenzmatrix und eine Dataframe, dass die Crash Rates und Attribute hält.

---
+++ geoprocessing_NewYork.py
@@ -0,0 +1,141 @@
+######NOTE#####
+## Only working if ARCGIS Pro is installed
+
+import arcpy
+
+#Used shapefiles
+trafficShapeFile = r'.\data\NewYork2017.shp'
+crashCSVFile = r'.\data\NYPD_Motor_Vehicle_Collisions_Clean.csv'
+crashShapeFile = r'.\data\NewYorkCrashes.shp'
+
+mappingTrafficShapeFile = r'.\data\NewYork2017_mapping.shp'
+segmentShapeFile = r'.\data\Segment_NewYork2017_FullBlob.shp'
+segmentShapeFile2 = r'.\data\Segment_NewYork2017_Intersected.shp'
+segmentShapeFile3 = r'.\data\Segment_NewYork2017_Mapped.shp'
+segmentShapeFile3Name = "Segment_NewYork2017_Mapped"
+
+
+
+
+#Import crash data
+XPoints = "LONGITUDE"
+YPoints = "LATITUDE"
+
+arcpy.MakeXYEventLayer_management(crashCSVFile, XPoints , YPoints , "New York CrashData","", "")
+arcpy.CopyFeatures_management("New York CrashData", crashShapeFile)
+
+#Clean traffic shape files
+#Delete everything besides data from Manhattan
+with arcpy.da.UpdateCursor(trafficShapeFile, "County_Cod") as cursor:
+    for row in cursor:
+        if row[0] != 61:
+            cursor.deleteRow()
+
+
+#Create mapping traffic shape file -> this file is used to later correctly map the AADTs
+#Background: some segments are to long or to short, that leads to unwanted mini segments
+#Split at intersections
+arcpy.FeatureToLine_management(trafficShapeFile,
+                               mappingTrafficShapeFile,
+                               "0.001 Meters")
+
+
+#Create segment shape file
+arcpy.Dissolve_management(mappingTrafficShapeFile, segmentShapeFile)
+	##Now we have one big connected polygon
+
+#Intersect segment shape file
+arcpy.FeatureToLine_management(segmentShapeFile,
+                              segmentShapeFile2,
+                               "0.001 Meters")
+
+#Add fields segment shape file, length and id
+arcpy.AddField_management(in_table= segmentShapeFile2, field_name = "Length_m", field_type="DOUBLE")
+arcpy.AddField_management(in_table= segmentShapeFile2, field_name = "Segment_ID", field_type="DOUBLE")
+
+
+#Calculate segment length of splitted traffic shape file
+arcpy.CalculateGeometryAttributes_management (in_features = segmentShapeFile2, geometry_property = [["Length_m", "LENGTH_GEODESIC"]], length_unit = "METERS")
+#Calculate segment ID
+arcpy.CalculateField_management (in_table = segmentShapeFile2, field = "Segment_ID", expression="!FID!")
+
+
+
+#Spatial join segment file with traffic mapping file
+fieldmappings = arcpy.FieldMappings()
+fieldmappings.addTable(segmentShapeFile2)
+fieldmappings.addTable(mappingTrafficShapeFile)
+
+arcpy.SpatialJoin_analysis (target_features = segmentShapeFile2, join_features = mappingTrafficShapeFile, 
+out_feature_class = segmentShapeFile3, 
+join_operation = "JOIN_ONE_TO_ONE", join_type = "KEEP_ALL", field_mapping = fieldmappings, 
+match_option ="SHARE_A_LINE_SEGMENT_WITH")
+
+
+#Spatial join crash 
+#Use distance to determine if map is valid or not!
+#Avoid double attaching of 
+outName = r'.\data\NewYorkCrashes_Mapped.shp'
+
+fieldmappings = arcpy.FieldMappings()
+
+# Add all fields from inputs.
+fieldmappings.addTable(segmentShapeFile3)
+fieldmappings.addTable(crashShapeFile)
+
+# Fields that shall remain
+keepers = ["Segment_ID","join_dist"] 
+
+# Remove all output fields you don't want.
+for field in fieldmappings.fields:
+    if field.name not in keepers:
+        fieldmappings.removeFieldMap(fieldmappings.findFieldMapIndex(field.name))
+
+
+
+arcpy.SpatialJoin_analysis (target_features = crashShapeFile, join_features = segmentShapeFile3, 
+out_feature_class = outName, 
+join_operation = "JOIN_ONE_TO_ONE", join_type = "KEEP_ALL", field_mapping = fieldmappings, 
+match_option ="CLOSEST", search_radius="0.0002",distance_field_name ="join_dist")
+
+
+
+
+##Create points at intersects -> mistakes can occur with bridges etc.
+intersectionShapeFile = r'.\data\Intersections.shp'
+arcpy.Intersect_analysis (in_features =segmentShapeFile3Name, out_feature_class=intersectionShapeFile, join_attributes="ONLY_FID", output_type="POINT")
+#Create point id
+#Add fields segment shape file, length and id
+arcpy.AddField_management(in_table= intersectionShapeFile, field_name = "Intsec_ID", field_type="Long")
+
+#Calculate intersection id
+arcpy.CalculateField_management (in_table = intersectionShapeFile, field = "Intsec_ID", expression="!FID!")
+
+
+
+
+##Map points to cornering roads
+joinedIntersectionsShapeFile = crashShapeFile = r'.\data\Adjacency.shp'
+fieldmappings = arcpy.FieldMappings()
+
+# Add all fields from inputs.
+fieldmappings.addTable(intersectionShapeFile)
+fieldmappings.addTable(segmentShapeFile3)
+
+# Fields that shall remain
+keepers = ["Intsec_ID","Segment_ID"] 
+
+# Remove all output fields you don't want.
+for field in fieldmappings.fields:
+    if field.name not in keepers:
+        fieldmappings.removeFieldMap(fieldmappings.findFieldMapIndex(field.name))
+
+
+arcpy.SpatialJoin_analysis (target_features = intersectionShapeFile
+, join_features = segmentShapeFile3, field_mapping=fieldmappings,
+out_feature_class = joinedIntersectionsShapeFile, 
+join_operation = "JOIN_ONE_TO_MANY", join_type = "KEEP_ALL",
+match_option ="INTERSECT")
+
+
+
---
+++ import_data.py
@@ -0,0 +1,78 @@
+import geopandas as gpd
+import pandas as pd
+import numpy as np
+import itertools
+
+#Use crash data to later infere crash counts
+mappedCrashData = gpd.read_file(r'data\NewYorkCrashes_Mapped.shp') 
+#Delete values with join_dist = -1 -> they are not mapped
+mappedCrashDataClean = mappedCrashData.loc[mappedCrashData['join_dist'] != -1]
+
+# Use to create adjaceny matrix
+adjacencyData = gpd.read_file(r'data\Adjacency.shp')
+
+#Map the crash counts to the segment data
+segmentData =  gpd.read_file(r'data\Segment_NewYork2017_Mapped.shp')
+#segmentData['Segment_ID'] = segmentData['Segment_ID'].astype(int)
+#Remove unnecessary columns
+segmentData = segmentData.drop(columns=['Join_Count', 'TARGET_FID', 'FID_Segmen', 
+                 'Id', 'FID_NewYor', 'Year_Recor', 'State_Code', 'Route_ID', 'Begin_Poin', 'End_Point', 
+                 'Route_Numb', 'Route_Name', 'Urban_Code', 'County_Cod',  'NHS', 'STRAHNET', 'Truck_NN', 
+                 'AADT_Singl', 'AADT_Combi', 'Toll_Charg', 'Toll_ID', 'Toll_Type', 'ESRI_OID', 'Shape_Leng',
+                 'geometry',])
+
+segmentData = segmentData.rename(columns={'F_System': 'Fun_Class',
+                           'Facility_T': 'Facility_Type',
+                           'F_System': 'Fun_Class',
+                           'Speed_Limi': 'Speed_Limit',
+                           'Access_Con': 'Access_Control'
+                           })
+
+#Set speed limits to 25 due to Manhattans wide 25 mph regulatory
+segmentData["Speed_Limit"] = segmentData["Speed_Limit"].replace(0,25)
+
+
+###############################################################################################################################
+#Map crash amounts to segements
+#Infere crashes per segment
+
+#Group crashes by Segment
+groupedCrashes = mappedCrashDataClean.groupby('Segment_ID').count()
+
+#Create array that holds all crashes
+crashArray= np.zeros((len(segmentData), 1))
+
+#Create list with crashes per segment
+for index, row in groupedCrashes.iterrows():
+    crashArray[int(index)] = row["Join_Count"]
+
+#Attach crash list to segments data
+segmentData['Crashes'] = crashArray.astype(int)
+
+#Calculate crash rate
+crashRate = segmentData['Crashes']/(segmentData['AADT']*(segmentData['Length_m']*1000)*365/1000000)
+segmentData['CrashRate'] = crashRate
+
+
+###############################################################################################################################
+#Create adjacency matrix
+n_segments = len(segmentData)
+adjacencyMatrix = np.zeros((n_segments, n_segments))
+intersection_list = adjacencyData["Intsec_ID"].unique().tolist()
+
+#Loop over all intersections
+for i in range(len(intersection_list)):
+    #Get all segments that are connected via this intersection
+    intersec = intersection_list[i]
+    
+    connectedSegments = adjacencyData.loc[adjacencyData['Intsec_ID'] == intersec]["Segment_ID"].tolist()   
+    #Combine all segments to tuples
+    combineSegments = list(itertools.permutations(connectedSegments, 2))
+    #Loop over all tuples and set to 1 (connection between these to segments)   
+    for j in range(len(combineSegments)):
+        tup = combineSegments[j]
+        #print(tup)
+        adjacencyMatrix[int(tup[0]),int(tup[1])] = 1
+        
+#######################################################################################################
+#Use segmentData and adjacencyMatrix

ebf44e240c270f10ce98de2be587a16bda3bfa6d
LyteFM <lytefm@live.de>
2019-07-02 10:57:41 +0200

    some refactoring

--- import_data.py
+++ import_data.py
@@ -6,7 +6,7 @@ import numpy as np
 import itertools
 import networkx as nx
 from pathlib import Path
-from pystan import StanModel, check_hmc_diagnostics
+from pystan import StanModel
 from sklearn.preprocessing import MaxAbsScaler
 from joblib import dump
 
@@ -149,73 +149,59 @@ if empty_row_count > 0:
 ###
 # model begins here
 ###
-filtered_df = segmentDF
-filtered_matrix = adjacencyMatrix
-# filtered_df['ones'] = np.ones(filtered_df.shape[0]) # not useful with QR
+
+def get_tobit_dict(filtered_df: pd.DataFrame, filtered_matrix):
+    trans = MaxAbsScaler().fit_transform(filtered_df[predictors + ['CrashRate']])
+    data_centered = pd.DataFrame(trans, columns=predictors + ['CrashRate'])
+    threshold = 0.0000000001
+    is_cens = data_centered['CrashRate'] < threshold
+    not_cens = data_centered['CrashRate'] >= threshold
+    ii_obs = filtered_df[not_cens].index + 1
+    ii_cens = filtered_df[is_cens].index + 1
+    tobit_dict = {'n_obs': not_cens.sum(), 'n_cens': is_cens.sum(), 'p': len(predictors),
+                  'ii_obs': ii_obs, 'ii_cens': ii_cens,
+                  'y_obs': filtered_df[not_cens]['CrashRate'], 'U': threshold,
+                  'X': filtered_df[predictors]}
+    return tobit_dict
+
+def run_tobit_model(tobit_dict, tobit_model):
+    tobit_params = {'adapt_delta': 0.95, 'max_treedepth': 15}
+    tobit_fit = tobit_model.sampling(data=tobit_dict, iter=5000, warmup=500,
+                                     control=tobit_params)
+    tobit_info = tobit_fit.stansummary()
+    with open(Path('data/crash_tobit_QR_gam.log'), 'w') as t_log:
+        t_log.write(tobit_info)
+    dump(tobit_fit, Path('data/crash_tobit_QR_gam.joblib'))
+    return tobit_fit
+
+def run_car_model(tobit_dict, car_model, filtered_matrix):
+    c_params = {'adapt_delta': 0.95, 'max_treedepth': 15}
+    car_dict = tobit_dict.copy()
+    car_dict['W'] = filtered_matrix
+    car_dict['W_n'] = filtered_matrix.sum()//2
+    car_dict['n'] = tobit_dict['X'].shape[0]
+    car_fit = car_model.sampling(data=car_dict, iter=5000, warmup=500, 
+                                 control=c_params, check_hmc_diagnostics=True)
+    car_info = car_fit.stansummary()
+    with open('data/crash_car_qr_5000.log', 'w') as c_log:
+        c_log.write(car_info)
+    dump(car_fit, 'data/crash_car_qr_5000.joblib')
+    return car_fit
+
+
+# TOBIT MODEL:
+# running for ~5h with n=5000 did not give the warnings anymore! Just need higher n!
 tobit_model = StanModel(file=Path('models/crash_tobit.stan').open(),
                         extra_compile_args=["-w"])
+tobit_dict = get_tobit_dict(segmentDF, adjacencyMatrix)
+tobit_fit = run_tobit_model(tobit_dict, tobit_model, adjacencyMatrix)
+
+# SPATIAL TOBIT MODEL:
 car_model = StanModel(file=Path('models/crash_CAR.stan').open(),
                       extra_compile_args=["-w"])
-
-trans = MaxAbsScaler().fit_transform(filtered_df[predictors + ['CrashRate']])
-data_centered = pd.DataFrame(trans, columns=predictors + ['CrashRate'])
-threshold = 0.0000000001
-is_cens = data_centered['CrashRate'] < threshold
-not_cens = data_centered['CrashRate'] >= threshold
-ii_obs = filtered_df[not_cens].index + 1
-ii_cens = filtered_df[is_cens].index + 1
-tobit_dict = {'n_obs': not_cens.sum(), 'n_cens': is_cens.sum(), 'p': len(predictors),
-              'ii_obs': ii_obs, 'ii_cens': ii_cens,
-              'y_obs': filtered_df[not_cens]['CrashRate'], 'U': threshold,
-              'X': filtered_df[predictors]}
-tobit_params = {'adapt_delta': 0.95, 'max_treedepth': 15}
-tobit_fit = tobit_model.sampling(data=tobit_dict, iter=1000, warmup=500,
-                                 control=tobit_params)
-tobit_info = tobit_fit.stansummary()
-with open(Path('data/crash_tobit_QR_gam.log'), 'w') as t_log:
-    t_log.write(tobit_info)
-dump(tobit_fit, Path('data/crash_tobit_QR_gam.joblib'))
-
-# 95% divergence
-# 5% max depth of 10
-# low E-BFMI (0.039 - 0.07)
-
-# now with adapt_delta: 0.9 and depth: 12
-# 89% divergence
-# 10% max depth
-# low E-BFMI (0.005 - 0.03)
-
-# seems like reparametrisation is really necessary...
-
-# as comparison: without the vectorisation
-# t_old_dict = {'n': data_centered.shape[0], 'p': len(predictors), 'X': filtered_df[predictors],
-#              'y': filtered_df['CrashRate'], 'U': threshold, 'n_cens': is_cens.sum()}
-# t_old_model = StanModel(file=Path('models/crash_tobit_old.stan').open(),
-#                        extra_compile_args=["-w"])
-#t_old_fit = t_old_model.sampling(data=t_old_dict, iter=20000, warmup=4000)
-#dump(t_old_fit, Path('data/crash_old.joblib'))
-#old_info = t_old_fit.stansummary()
-# with open(Path('data/crash_old.log'), 'w') as old_log:
-#    old_log.write(old_info)
-# 96% div
-# 3.8% max tree depth
-# low E-BFMI (0.36 - 0.08)
-# but nice. The parameters β are roughly same. Vectorisation seems OK.
-
-
-# WARNING: this will take ages for large iter.
-c_params = {'adapt_delta': 0.95, 'max_treedepth': 15}
-car_dict = tobit_dict.copy()
-car_dict['W'] = filtered_matrix
-car_dict['W_n'] = filtered_matrix.sum()//2
-car_dict['n'] = data_centered.shape[0]
-car_fit = car_model.sampling(
-    data=car_dict, iter=1000, warmup=500, control=c_params)
-car_info = car_fit.stansummary()
-with open('data/crash_car_qr.log', 'w') as c_log:
-    c_log.write(car_info)
-dump(car_fit, 'data/car_tobit_qr.joblib')
+car_fit = run_car_model(tobit_dict, car_model)
 # WARNING: E-BFMI 0.001 - 0.06
+# I could also run more chains -> Have 8 threads available if I just let run overnight
 
 
 ########################################################################################################################
